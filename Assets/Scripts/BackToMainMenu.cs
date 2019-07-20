c45f046d0d40ff394063dde8caaa3980067dd204
Osama Alsalman <oalsalman@protonmail.com>
2019-07-20 14:24:26 +0300

    Added back to main menu button from spell selection

---
+++ Assets/Scripts/BackToMainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BackToMainMenu : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(() => { SceneManager.LoadScene(1); });
+    }
+}
