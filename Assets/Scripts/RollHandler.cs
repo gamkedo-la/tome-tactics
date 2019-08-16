f2822320bd78033b3938acf6e9fecbe072ff8dc1
DESKTOP-N37AKD8\Dominick Aiudi <dra17d@my.fsu.edu>
2019-08-16 10:57:30 -0400

    Show correct number of dice results and prevent dice from falling off of the table

--- Assets/Scripts/RollHandler.cs
+++ Assets/Scripts/RollHandler.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RollHandler : MonoBehaviour
 {
-	[SerializeField] private GameObject die;
+	[SerializeField] private GameObject diePrefab;
 	private GameObject[] dieInst = null;
 	private Rigidbody rig;
 
@@ -17,14 +17,15 @@ public class RollHandler : MonoBehaviour
 
         foreach (GameObject die in dieInst)
         {
+            if (die == null)
+                continue;
+
             rig = die.GetComponent<Rigidbody>();
             if (rig != null)
             {
                 if (rig.IsSleeping())
                 {
-                    getResult();
-                    rig = null;
-                    dieInst = null;
+                    getResult(die);
                 }
             }
         }
@@ -32,13 +33,13 @@ public class RollHandler : MonoBehaviour
 
     public void rollDice(int numOfDice = 1)
     {
-    	Vector3 pos = new Vector3(5.0f, 7.5f, 5.0f);
-
         dieInst = new GameObject[numOfDice];
 
         for (int i = 0; i < numOfDice; ++i)
         {
-            dieInst[i] = Instantiate(die, pos, Random.rotation);
+            Vector3 pos = new Vector3(5.0f, 7.5f + i, 5.0f);
+
+            dieInst[i] = Instantiate(diePrefab, pos, Random.rotation);
             rig = dieInst[i].GetComponent<Rigidbody>();
 
             rig.AddForce(-5.0f, 0.0f, -5.0f, ForceMode.Impulse);
@@ -48,7 +49,7 @@ public class RollHandler : MonoBehaviour
         }
     }
 
-    public void getResult()
+    public void getResult(GameObject die)
     {
     	int diceCount = 0;
 
@@ -56,25 +57,20 @@ public class RollHandler : MonoBehaviour
     	// Debug.Log(Vector3.Dot (dieInst.transform.up, Vector3.up));
     	// Debug.Log(Vector3.Dot (dieInst.transform.right, Vector3.up));
 
-        foreach (GameObject die in dieInst)
-        {
+    	if (Vector3.Dot (die.transform.forward, Vector3.up) > 0.9)
+    		diceCount = 4; // 5
+    	else if (Vector3.Dot (-die.transform.forward, Vector3.up) > 0.9)
+    		diceCount = 5; // 2
+    	else if (Vector3.Dot (die.transform.up, Vector3.up) > 0.9)
+    		diceCount = 3; // 3
+    	else if (Vector3.Dot (-die.transform.up, Vector3.up) > 0.9)
+    		diceCount = 1; // 4
+    	else if (Vector3.Dot (die.transform.right, Vector3.up) > 0.9)
+    		diceCount = 6; // 6
+    	else if (Vector3.Dot (-die.transform.right, Vector3.up) > 0.9)
+    		diceCount = 2; // 1
 
-        	if (Vector3.Dot (die.transform.forward, Vector3.up) > 0.9)
-        		diceCount = 4; // 5
-        	else if (Vector3.Dot (-die.transform.forward, Vector3.up) > 0.9)
-        		diceCount = 5; // 2
-        	else if (Vector3.Dot (die.transform.up, Vector3.up) > 0.9)
-        		diceCount = 3; // 3
-        	else if (Vector3.Dot (-die.transform.up, Vector3.up) > 0.9)
-        		diceCount = 1; // 4
-        	else if (Vector3.Dot (die.transform.right, Vector3.up) > 0.9)
-        		diceCount = 6; // 6
-        	else if (Vector3.Dot (-die.transform.right, Vector3.up) > 0.9)
-        		diceCount = 2; // 1
-
-        	Debug.Log ("diceCount :" + diceCount);
-            Destroy(die);
-        }
-
+    	Debug.Log ("diceCount :" + diceCount);
+        Destroy(die);
     }
 }
