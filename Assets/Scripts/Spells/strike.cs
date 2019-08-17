196074c918077438f2de8c244ab6fd9481ad6082
DESKTOP-N37AKD8\Dominick Aiudi <dra17d@my.fsu.edu>
2019-08-17 12:51:01 -0400

    Implement lightning damage dice and reroll "cocked"/inconclusive dice

--- Assets/Scripts/Spells/strike.cs
+++ Assets/Scripts/Spells/strike.cs
@@ -4,22 +4,43 @@ using UnityEngine;
 
 public class strike : MonoBehaviour
 {
+    private RollHandler roller;
     private ParticleSystem system;
+    private casterScript enemy = null;
 
-    void Start() { system = GetComponent<ParticleSystem>(); }
+    void Start()
+    {
+        system = GetComponent<ParticleSystem>();
+
+        GameObject input = GameObject.Find("Input and Game Logic");
+        roller = input.GetComponent<RollHandler>();
+    }
 
     void Update()
     {
-        if (!system.IsAlive()) { Destroy(gameObject); }
+        if (!system.IsAlive() && enemy == null)
+        {
+            Destroy(gameObject); 
+        }
+        else
+        {
+            int dam = roller.isDone();
+            if (dam != 0)
+            {
+                enemy.takeDamage(dam);
+                roller.Reset();
+                Destroy(gameObject);
+            }
+        }
     }
 
     void OnCollisionEnter(Collision coll)
     {
-    	// Debug.Log("OnEnter ran");
-		if (coll.collider.tag == "Opponent" || coll.collider.tag == "Player")
+    	// Debug.Log("OnEnter ran " + coll.collider.tag);
+		if (coll.collider.tag == "Player2" || coll.collider.tag == "Player")
 		{
-			casterScript sucker = coll.gameObject.GetComponent<casterScript>();
-			sucker.takeDamage(20);
+            roller.rollDice(2);
+			enemy = coll.gameObject.GetComponent<casterScript>();
 		}
     }
 }
